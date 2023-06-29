56243a5a5d85a44fbc56a254a5ba754c34d78baf
Seongwon <wjdtjddnjs99@kaist.ac.kr>
2023-06-29 20:41:07 +0900

    시작

---
+++ app/src/main/java/com/example/week1_android_studio/GalleryFragment.java
@@ -0,0 +1,23 @@
+package com.example.week1_android_studio;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/week1_android_studio/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.week1_android_studio;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment;
+    GalleryFragment galleryFragment;
+    FreeFragment freeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        galleryFragment = new GalleryFragment();
+        freeFragment = new FreeFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                if(item.getItemId() == R.id.contacts){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFragment).commit();
+                    return true;
+                } else if (item.getItemId() == R.id.photo) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, galleryFragment).commit();
+                    return true;
+                }
+                else if (item.getItemId() == R.id.free) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.containers, freeFragment).commit();
+                    return true;
+                }
+                else return false;
+            }
+        });
+    }
+}
