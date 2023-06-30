8e843dbbf2146997a6f248f314a12cad6d6a78de
2021059507 <2021059507@hanyang.ac.kr>
2023-06-30 15:29:03 +0900

    home transition

--- app/src/main/java/com/example/week1_android_studio/HomeFragment.java
+++ app/src/main/java/com/example/week1_android_studio/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.week1_android_studio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,7 +15,7 @@ import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the  factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
@@ -24,11 +26,11 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
+        
         initUI(rootView);
 
         return rootView;
     }
-
     private void initUI(ViewGroup rootView){
         ArrayList<String> testDataSet = new ArrayList<>();
         for (int i = 0; i<20; i++) {
@@ -41,6 +43,16 @@ public class HomeFragment extends Fragment {
         recyclerView.setLayoutManager(layoutManager);
 
         CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+
+        //click event implementation
+        customAdapter.setOnItemclickListener(new CustomAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int position, String data) {
+                //여기서 다음 탭으로 넘어가면...되지 않을까?
+                Intent intent = new Intent(getContext(), HomeInfoActivity.class);
+                startActivity(intent);
+            }
+        });
         recyclerView.setAdapter(customAdapter);
     }
 }
---
+++ app/src/main/java/com/example/week1_android_studio/HomeInfoActivity.java
@@ -0,0 +1,13 @@
+package com.example.week1_android_studio;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homeinfo);
+    }
+}
